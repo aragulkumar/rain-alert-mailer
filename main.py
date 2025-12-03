4c03087bfbfd09b053e275b6a743c04f3e13198b
Ragul Kumar A <aragulkumar11@gmail.com>
2025-12-03 13:30:36 +0000

    completed the project with sms system, you can add it in pythonanywhere to automate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime
-from twilio.rest import client
+from twilio.rest import Client
+import os
 
 #Chennai
 LAT = 13.0827
@@ -18,6 +19,24 @@ Weather_params = {
     "timezone": "auto"
 }
 
+WORK_START_HOUR = 8
+WORK_END_HOUR = 18
+
+#--------------------------------
+
+#TWILIO DETAILS
+# TWILIO_SID = "AC7a31917d221053bede"
+# TWILIO_AUTH_TOKEN = "5f65c578ab8f5c"
+# TWILIO_PHONE_NUMBER = "+151549"    # Your Twilio number
+# TARGET_PHONE_NUMBER = "+91956" 
+
+TWILIO_SID = os.getenv("TWILIO_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
+TARGET_PHONE_NUMBER = os.getenv("TARGET_PHONE_NUMBER")
+
+
+
 
 def will_it_rain_during_work_hours(start_hour=8,end_hour=18):
 
@@ -30,7 +49,6 @@ def will_it_rain_during_work_hours(start_hour=8,end_hour=18):
 
     today = datetime.now().date()
 
-    is_rain = False
 
     for time_str, rain_mm in zip(times, precipitation):
         dt = datetime.fromisoformat(time_str)
@@ -39,6 +57,21 @@ def will_it_rain_during_work_hours(start_hour=8,end_hour=18):
         if dt.date() == today and start_hour <= dt.hour < end_hour:
             if rain_mm > 0:
 
-                is_rain = True
+                return True
+    return False
 
+
+def send_sms(message_body):
+    client = Client(TWILIO_SID,TWILIO_AUTH_TOKEN)
+    message = client.messages.create(
+        body = message_body,
+        from_=TWILIO_PHONE_NUMBER,
+        to=TARGET_PHONE_NUMBER
+    )
+    print("SMS sent. Twilio status:", message.status)
             
+if __name__ == "__main__":
+    if will_it_rain_during_work_hours(WORK_START_HOUR, WORK_END_HOUR):
+        send_sms("☔ RAIN ALERT: It will rain sometime between 8 AM and 6 PM. Take an umbrella!")
+    else:
+        send_sms("No rain in work hours. No SMS sent.")
